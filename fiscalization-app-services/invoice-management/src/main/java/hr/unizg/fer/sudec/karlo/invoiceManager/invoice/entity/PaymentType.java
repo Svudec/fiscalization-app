2ed12b57e4b8a9e9fc9aba754bfff65ad6959217
Karlo Sudec <karlo.sudec@gmail.com>
2023-06-18 04:25:06 +0200

    add more payment types

--- fiscalization-app-services/invoice-management/src/main/java/hr/unizg/fer/sudec/karlo/invoiceManager/invoice/entity/PaymentType.java
+++ fiscalization-app-services/invoice-management/src/main/java/hr/unizg/fer/sudec/karlo/invoiceManager/invoice/entity/PaymentType.java
@@ -1,9 +1,13 @@
 package hr.unizg.fer.sudec.karlo.invoiceManager.invoice.entity;
 
 public enum PaymentType{
-    GOTOVINA("G"), KARTICA("K"), OSTALO("O");
+    GOTOVINA("G"),
+    KARTICA("K"),
+    CEK("C"),
+    TRANSAKCIJSKI_RACUN("T"),
+    OSTALO("O");
     private final String code;
-    private PaymentType(String code){
+    PaymentType(String code){
         this.code = code;
     }
     public String getCode() {
